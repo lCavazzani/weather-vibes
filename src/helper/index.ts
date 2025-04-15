07d062ac9c6122b3322c01224ae43244df232cb0
leonardo-cavazzani <cavazzanileonardo@gmail.com>
2025-04-15 11:54:40 -0300

    env file

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -2,4 +2,4 @@
 export const convertTemperature = (temp: number, unit: "C" | "F"): number => {
   return unit === "F" ? (temp * 9) / 5 + 32 : temp;
 };
-export const apiKey = "05e2b6d98d58551c9baf29a63fa5ad97"; // ← Replace this with your real key
+export const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
